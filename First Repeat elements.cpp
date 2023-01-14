70a98600231b3a9626c4b56e5fc46ff413a32184
Tripti <114797483+barbiethecoder@users.noreply.github.com>
2023-01-15 00:49:19 +0530

    Create First Repeat elements.cpp

---
+++ First Repeat elements.cpp
@@ -0,0 +1,10 @@
+int Solution::solve(vector<int> &A) {
+    for(int i=0; i<A.size(); i++){
+        for(int j=i+1; j<A.size(); j++){
+            if(A[i]==A[j]){
+                return A[i];
+            }
+        }
+    }
+    return -1;
+}
